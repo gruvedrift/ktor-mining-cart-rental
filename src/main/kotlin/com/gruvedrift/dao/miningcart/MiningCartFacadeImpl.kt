a14eed525ae0fdfb0e126cbeb30040e16e79c1ba
gruvedrift <william.gruvedrift@gmail.com>
2023-12-26 23:26:23 +0100

    companion object for StorageSize to enforce set sizes. started implementing db queries

--- src/main/kotlin/com/gruvedrift/dao/miningcart/MiningCartFacadeImpl.kt
+++ src/main/kotlin/com/gruvedrift/dao/miningcart/MiningCartFacadeImpl.kt
@@ -1,11 +1,22 @@
 package com.gruvedrift.dao.miningcart
 
 import com.gruvedrift.models.MiningCart
+import com.gruvedrift.models.MiningCartTable
 import com.gruvedrift.models.StorageSize
+import org.jetbrains.exposed.sql.Min
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.selectAll
+import java.lang.IllegalArgumentException
 
 class MiningCartFacadeImpl : MiningCartFacade {
+
+    private fun resultRowToMiningCart(row: ResultRow): MiningCart {
+        val capacity = row[MiningCartTable.storageCapacity]
+        val storageSize = StorageSize.getByCapacity(capacity) ?: throw IllegalArgumentException("Illegal storage size: $capacity")
+        return MiningCart(id = 1, storageCapacity = storageSize)
+    }
     override suspend fun allCarts(): List<MiningCart> {
-        TODO("Not yet implemented")
+        return MiningCartTable.selectAll().map { resultRowToMiningCart(it) }
     }
 
     override suspend fun allCartsByCapacity(capacity: StorageSize): List<MiningCart>? {
--- src/main/kotlin/com/gruvedrift/models/MiningCart.kt
+++ src/main/kotlin/com/gruvedrift/models/MiningCart.kt
@@ -11,29 +11,32 @@ data class MiningCart(
 object MiningCartTable : Table() {
     val id = integer("id").autoIncrement()
     val orderId = integer("order_id").references(OrderTable.id).nullable()
-    val capacity = integer("capacity")
+    val storageCapacity = integer("storageCapacity")
 
     override val primaryKey = PrimaryKey(id)
 }
 
 enum class StorageSize(val capacity: Int) {
     SMALL(100),
-    MEDIUM(250),
+    MEDIUM (250),
     LARGE(400),
-    X_LARGE(600),
+    X_LARGE(600);
+
+    companion object  {
+       fun getByCapacity(capacity: Int): StorageSize? =
+           values().find { it.capacity == capacity}
+    }
 }
 
-val testMiningCarts = listOf<MiningCart>(
+val testMiningCarts = listOf(
     MiningCart(0, StorageSize.SMALL),
     MiningCart(1, StorageSize.MEDIUM),
     MiningCart(2, StorageSize.LARGE),
     MiningCart(3, StorageSize.X_LARGE),
 )
-val testMiningCartsTwo = listOf<MiningCart>(
+val testMiningCartsTwo = listOf(
     MiningCart(4, StorageSize.SMALL),
     MiningCart(5, StorageSize.LARGE),
     MiningCart(6, StorageSize.LARGE),
     MiningCart(7, StorageSize.SMALL),
 )
-
-
